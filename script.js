00f38757211622b219a29a27c5a887bc97af4dfe
orgmediaederak-alt <org.media.ederak@gmail.com>
2025-08-02 20:31:16 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const newsList = document.getElementById('news-list');
+    newsList.innerHTML = `
+        <li>تحلیل: تأثیر درگیری‌های خاورمیانه بر امنیت جهانی</li>
+        <li>یادداشت: راهبرد محور مقاومت در برابر اسرائیل</li>
+        <li>گزارش ویژه: موقعیت ژئوپلیتیک ایران در سال 2025</li>
+    `;
+});
